40c21eb40da2c04de6d43e6f455aa39e809311ec
Holms2009 <holms200990@gmail.com>
2022-02-02 18:59:16 +0300

    Added firebase

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 import App from './App.vue'
 
 Vue.config.productionTip = false
@@ -6,3 +8,16 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqqtJ13_9RQY2VcotGrDGr30nOZ6Y6e_8",
+  authDomain: "vue-task-board-8c5bd.firebaseapp.com",
+  projectId: "vue-task-board-8c5bd",
+  storageBucket: "vue-task-board-8c5bd.appspot.com",
+  messagingSenderId: "912348612221",
+  appId: "1:912348612221:web:8b6efbbf5ef3e4d7989314",
+  measurementId: "G-QKR9R2TY3E"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
